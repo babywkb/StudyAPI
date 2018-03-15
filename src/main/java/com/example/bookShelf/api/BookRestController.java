93d6b091da8462fd73089a0964de3e225a78f7ec
babywkb <TakataniHiroki@MacBookPro2.local>
2018-03-16 08:07:11 +0900

    サーバサイドはとりあえずCRUDできる

--- src/main/java/com/example/bookShelf/api/BookRestController.java
+++ src/main/java/com/example/bookShelf/api/BookRestController.java
@@ -3,8 +3,12 @@ package com.example.bookShelf.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +23,29 @@ public class BookRestController {
 
 	@GetMapping
 	List<Book> getBooks() {
-		List<Book> books = bookService.findAll();
+		final List<Book> books = bookService.findAll();
 		return books;
 	}
 
 	@GetMapping(path = "{id}")
-	Book getBook(@PathVariable Integer id) {
-		Book book = bookService.findOne(id);
+	Book getBook(@PathVariable final Integer id) {
+		final Book book = bookService.findOne(id);
 		return book;
 	}
 
+	@PostMapping
+	Book postBooks(@RequestBody final Book book) {
+		return bookService.create(book);
+	}
+
+	@PutMapping(path = "{id}")
+	Book putBook(@PathVariable final Integer id, @RequestBody final Book book) {
+		book.setId(id);
+		return bookService.update(book);
+	}
+
+	@DeleteMapping(path = "{id}")
+	void deleteBook(@PathVariable final Integer id) {
+		bookService.delete(id);
+	}
 }
